ab0d163ea2fe5bc7e8d345aa22494032f9429c4e
Aifuwa O.Roland <44173201+AifuwaRoland@users.noreply.github.com>
2018-12-29 05:50:59 -0600

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,13 @@
+﻿ const express =require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/frontend");
+//app.use(express.static(__dirname + "/frontend"));
+
+});
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+
+});
+    
